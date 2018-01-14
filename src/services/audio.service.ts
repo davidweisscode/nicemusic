c7e6afe8a66f5d122d97e572aeee03846a562d95
David Weiß <davidweisspost@gmail.com>
2018-01-14 22:51:06 +0100

    Add media status updates

--- src/services/audio.service.ts
+++ src/services/audio.service.ts
@@ -16,6 +16,22 @@ export class AudioService {
     this.audio = this.media.create(
       this.file.externalRootDirectory + item.fullPath
       );
+
+
+    this.audio.onStatusUpdate.subscribe(status => console.log("STATUS", status)); // fires when file status changes
+
+    this.audio.onSuccess.subscribe(() => console.log('Action is successful')); // Fires after audio finished playing
+
+    this.audio.onError.subscribe(error => console.log('Error!', error));
+
+    /*
+    Media.MEDIA_NONE = 0;
+    Media.MEDIA_STARTING = 1;
+    Media.MEDIA_RUNNING = 2;
+    Media.MEDIA_PAUSED = 3;
+    Media.MEDIA_STOPPED = 4;
+    */
+
   }
 
   playAudio() {
