c7bd985339aca7306f04eda33a55b2c534db543c
David Weiß <davidweisspost@gmail.com>
2019-04-27 17:01:48 +0200

    Read tags from online files

--- src/pages/archive/archive.ts
+++ src/pages/archive/archive.ts
@@ -6,6 +6,8 @@ import { AudioService } from "../../services/audio.service";
 import jsMediaTags from "jsmediatags";
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 
+declare let window: any;
+
 @Component({
   selector: "page-archive",
   templateUrl: "archive.html"
@@ -51,19 +53,45 @@ export class ArchivePage {
   }
 
   getTags() {
+    // https://github.com/ionic-team/cordova-plugin-ionic-webview/issues/193
     // ExtRootDir  file:///storage/emulated/0/
-    // NativeURL   file:///storage/emulated/0/Music/Negroman/Sequel%20EP/01.%20Nejromunn.mp3 // fileEntry
-    // InternalURL cdvfile://localhost/sdcard/Music/Negroman/Sequel%20EP/01.%20Nejromunn.mp3 //toInternalURL
+    // NativeURL   file:///storage/emulated/0/Music/Dramadigs/04.%20Scherben.mp3 //fileEntry
+    // InternalURL cdvfile://localhost/sdcard/Music/Dramadigs/04.%20Scherben.mp3 //toInternalURL
     this.file.resolveDirectoryUrl(this.file.externalRootDirectory).then((dirEntry) => {
+      console.log("DIR ENTRY", dirEntry);
 
-      this.file.getFile(dirEntry, "Music/Negroman/Sequel EP/01. Nejromunn.mp3", {create: false}).then((fileEntry) => {
+      this.file.resolveLocalFilesystemUrl("file:///storage/emulated/0/Music/Dramadigs/04.%20Scherben.mp3").then((fileEntry) => {
+        console.log("ResLocFileSysUrl", fileEntry);
+        // Local example
+        // https://github.com/aadsm/jsmediatags/issues/51 "No suitable reader found"
+        jsMediaTags.read(fileEntry, {
+          onSuccess: function(tag) {
+            console.log("TAG", tag);
+          },
+          onError: function(error) {
+            console.log("ERROR", error);
+          }
+        });
+      });
 
-        console.log("CONVERT_EXAMPLE", this.webView.convertFileSrc('file:///Users/dan/camera-image-12345.png'));
-        console.log("CONVERT_NATIVE", this.webView.convertFileSrc('file:///storage/emulated/0/Music/Negroman/Sequel%20EP/01.%20Nejromunn.mp3'));
-        console.log("CONVERT_INTERNAL", this.webView.convertFileSrc('cdvfile://localhost/sdcard/Music/Negroman/Sequel%20EP/01.%20Nejromunn.mp3'));
+      // Online example
+      jsMediaTags.read("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", {
+        onSuccess: function(tag) {
+          console.log("TAG", tag);
+        },
+        onError: function(error) {
+          console.log("ERROR", error);
+        }
+      });
 
+      /*this.file.getFile(dirEntry, "Music/Dramadigs/04.%20Scherben.mp3", {create: false}).then((fileEntry) => {
+        console.log("FILE ENTRY", fileEntry);
+        //console.log("CONVERT_EXAMPLE", this.webView.convertFileSrc('file:///Users/dan/camera-image-12345.png'));
+        //console.log("CONVERT_NATIVE", this.webView.convertFileSrc('file:///storage/emulated/0/Music/Dramadigs/04.%20Scherben.mp3'));
+        //console.log("CONVERT_INTERNAL", this.webView.convertFileSrc('cdvfile://localhost/sdcard/Music/Dramadigs/04.%20Scherben.mp3'));
+        console.log("CONVERT", this.convertFileSrc(fileEntry));
 
-        jsMediaTags.read("http://localhost:8080/_file_/" + "storage/emulated/0/Music/Negroman/Sequel%20EP/01.%20Nejromunn.mp3", {
+        jsMediaTags.read("http://localhost:8080/_file_/" + "storage/emulated/0/Music/Dramadigs/04.%20Scherben.mp3", {
           onSuccess: function(tag) {
             console.log(tag);
           },
@@ -71,11 +99,24 @@ export class ArchivePage {
             console.log(err, err.type, err.info);
           }
         });
-
-      });
-
+      });*/
     });
+  }
 
+  convertFileSrc(url: any) {
+    if (!url) {
+      return url;
+    }
+    if (url.startsWith('/')) {
+      return window.WEBVIEW_SERVER_URL + '/_app_file_' + url;
+    }
+    if (url.startsWith('file://')) {
+      return window.WEBVIEW_SERVER_URL + url.replace('file://', '/_app_file_');
+    }
+    if (url.startsWith('content://')) {
+      return window.WEBVIEW_SERVER_URL + url.replace('content:/', '/_app_content_');
+    }
+    return url;
   }
 
 }
